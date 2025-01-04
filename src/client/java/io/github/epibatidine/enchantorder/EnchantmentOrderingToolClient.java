e02d7553bc7a4099eefbe9a64d258992e5ea4f25
Epibatidine <catutorial7@gmail.com>
2025-01-04 11:03:11 +0100

    base of project

---
+++ src/client/java/io/github/epibatidine/enchantorder/EnchantmentOrderingToolClient.java
@@ -0,0 +1,10 @@
+package io.github.epibatidine.enchantorder;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnchantmentOrderingToolClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
